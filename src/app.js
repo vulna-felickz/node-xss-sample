356c8afbd8c8a8814e3c6e8849ad174ed04d0f74
felickz <1760475+felickz@users.noreply.github.com>
2024-03-02 22:35:45 -0500

    scenario 4 conversion to array + get rid of random other sink i didnt really want to be there.

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ const app = express();
 // Define your whitelist and queryAndParseAppSettings function
 const whitelist = ['key1', 'key2', 'key3'];
 const queryAndParseSettings = async (req, keys) => {
-  // Replace this with your own logic
-  return keys.reduce((acc, key) => {
-    acc[key] = `value-${key}`;
-    return acc;
-  }, {});
-
+  // // Replace this with your own logic, this is vulnerable to Remote property injection but should be gaurded by the whitelist check.
+  // return keys.reduce((acc, key) => {
+  //   acc[key] = `value-${key}`;
+  //   return acc;
+  // }, {});
+  return "doesnt matter";
 };
 
 
@@ -38,7 +38,7 @@ const getSettings = (whitelist) => async (req, res) => {
 // Use the middleware for the /settings route
 // Works: http://localhost:3000/settings/key2
 // Fails: http://localhost:3000/settings/boop
-// Vunlerablility: http://localhost:3000/settings/%3Cimg%20src=x%20onerror=alert(origin)%3E
+// Vunlerablility Not Found 🤔: http://localhost:3000/settings/%3Cimg%20src=x%20onerror=alert(origin)%3E
 app.get('/settings/:keys', getSettings(whitelist));
 
 // Vulnerable: http://localhost:3000/echo/%3Cimg%20src=x%20onerror=alert(origin)%3E
@@ -70,6 +70,23 @@ const echo3 = (req, res) => {
 // NotVulnerable: http://localhost:3000/echo3/abcd
 // Vulnerable: http://localhost:3000/echo3/%3Cimg%20src=x%20onerror=alert(origin)%3E
 app.get('/echo3/:message', echo3);
+
+const echo4 = (req, res) => {
+  const { keys: queryKeys } = req.query;
+  const paramKeys = req.params;
+  const message = queryKeys || paramKeys?.keys;
+
+  const messageArray = typeof message === 'string' ? [message] : message;
+  // use the filter method on a list of defined strings "test" and "test2" to check if the message is in the list, otherwise return the message
+  var notmatch = messageArray.filter((test) => !test.includes("test") && !test.includes("test2") );
+
+  res.send("notmatch: " + notmatch + " message: " + message);
+};
+
+// NotVulnerable: http://localhost:3000/echo4/test
+// NotVulnerable: http://localhost:3000/echo4/abcd
+// Vulnerable: http://localhost:3000/echo4/%3Cimg%20src=x%20onerror=alert(origin)%3E
+app.get('/echo4/:keys', echo4);
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!');
