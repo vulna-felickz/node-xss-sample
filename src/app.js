c879ee8b84a13bbcb179aac06f3aa36d4076d503
felickz <1760475+felickz@users.noreply.github.com>
2024-03-02 23:33:52 -0500

    Simplify example - 2 scenarios - 1 with a middleware that requires a parameter passed into function handler as closure - 1 with a middleware that has no params

--- src/app.js
+++ src/app.js
@@ -2,17 +2,13 @@ const express = require('express');
 const app = express();
 
 // Define your whitelist and queryAndParseAppSettings function
-const whitelist = ['key1', 'key2', 'key3'];
+const global_whitelist = ['key1', 'key2', 'key3'];
 const queryAndParseSettings = async (req, keys) => {
-  // // Replace this with your own logic, this is vulnerable to Remote property injection but should be gaurded by the whitelist check.
-  // return keys.reduce((acc, key) => {
-  //   acc[key] = `value-${key}`;
-  //   return acc;
-  // }, {});
-  return "doesnt matter";
+  return "OK - matches value in whitelist:" + global_whitelist.join(", ");
 };
 
 
+//Middleware that accepts a parameter as a closure
 const getSettings = (whitelist) => async (req, res) => {
   const { keys: queryKeys } = req.query;
   const paramKeys = req.params;
@@ -34,71 +30,101 @@ const getSettings = (whitelist) => async (req, res) => {
   res.json(results);
 };
 
-
-// Use the middleware for the /settings route
-// Works: http://localhost:3000/settings/key2
-// Fails: http://localhost:3000/settings/boop
-// Vunlerablility Not Found 🤔: http://localhost:3000/settings/%3Cimg%20src=x%20onerror=alert(origin)%3E
-app.get('/settings/:keys', getSettings(whitelist));
-
-// Vulnerability: http://localhost:3000/echo/%3Cimg%20src=x%20onerror=alert(origin)%3E
-app.get('/echo/:message', (req, res) => {
-  const { message } = req.params;
-  res.send(message);
-});
-
-const echo2 = (req, res) => {
-  const { message } = req.params;
-  res.send(message);
-};
-
-// Vulnerability: http://localhost:3000/echo2/%3Cimg%20src=x%20onerror=alert(origin)%3E
-app.get('/echo2/:message', echo2);
-
-
-const echo3 = (req, res) => {
-  const { message } = req.params;
-
-  const messageArray = typeof message === 'string' ? [message] : message;
-  // use the filter method on a list of defined strings "test" and "test2" to check if the message is in the list, otherwise return the message
-  var notmatch = messageArray.filter((test) => !test.includes("test") && !test.includes("test2") );
-
-  res.send("notmatch: " + notmatch + " message: " + message);
-};
-
-// NotVulnerable: http://localhost:3000/echo3/test
-// NotVulnerable: http://localhost:3000/echo3/abcd
-// Vulnerability: http://localhost:3000/echo3/%3Cimg%20src=x%20onerror=alert(origin)%3E
-app.get('/echo3/:message', echo3);
-
-const echo4 = (unusedparam) => (req, res) => {
+//Middleware that does not have any parameters - uses global variable instead
+const getSettings2 =  async (req, res) => {
   const { keys: queryKeys } = req.query;
   const paramKeys = req.params;
-  const message = queryKeys || paramKeys?.keys;
+  const keys = queryKeys || paramKeys?.keys;
 
-  if (!message) {
+  if (!keys) {
     res.status(400).send('No keys provided');
     return;
   }
+  const keyArray = typeof keys === 'string' ? [keys] : keys;
+  const invalidKeys = keyArray.filter(key => !global_whitelist.includes(key));
 
-
-  const messageArray = typeof message === 'string' ? [message] : message;
-  // use the filter method on a list of defined strings "test" and "test2" to check if the message is in the list, otherwise return the message
-  var notmatch = messageArray.filter((test) => !test.includes("test") && !test.includes("test2") );
-
-  if (notmatch.length) {
-    res.send("notmatch: " + notmatch + " message: " + message);
+  if (invalidKeys.length) {
+    res.status(400).send(`${invalidKeys.join(', ')} not in whitelist`);
     return;
   }
+  const results = await queryAndParseSettings(req, keyArray);
 
-  res.send("OK! Whitelist:"+unusedparam);
-
+  res.json(results);
 };
 
-// OK - in whitelist: http://localhost:3000/echo4/test
-// NOT - in whitelist: http://localhost:3000/echo4/abcd
-// Vulnerability: http://localhost:3000/echo4/%3Cimg%20src=x%20onerror=alert(origin)%3E
-app.get('/echo4/:keys', echo4(whitelist));
+
+// Use the middleware for the /settings route
+// 200: http://localhost:3000/settings/key2
+// 400: http://localhost:3000/settings/boop
+// 400 but Vunlerablility Not Found 🤔: http://localhost:3000/settings/%3Cimg%20src=x%20onerror=alert(origin)%3E
+app.get('/settings/:keys', getSettings(global_whitelist));
+
+// Use the middleware for the /settings route
+// Works: http://localhost:3000/settings2/key2
+// Fails: http://localhost:3000/settings2/boop
+// 400 Vunlerablility FOUND 🤪: http://localhost:3000/settings2/%3Cimg%20src=x%20onerror=alert(origin)%3E
+app.get('/settings2/:keys', getSettings2);
+
+
+// OTHER TESTING - Everything was fine
+// // Vulnerability: http://localhost:3000/echo/%3Cimg%20src=x%20onerror=alert(origin)%3E
+// app.get('/echo/:message', (req, res) => {
+//   const { message } = req.params;
+//   res.send(message);
+// });
+
+// const echo2 = (req, res) => {
+//   const { message } = req.params;
+//   res.send(message);
+// };
+
+// // Vulnerability: http://localhost:3000/echo2/%3Cimg%20src=x%20onerror=alert(origin)%3E
+// app.get('/echo2/:message', echo2);
+
+
+// const echo3 = (req, res) => {
+//   const { message } = req.params;
+
+//   const messageArray = typeof message === 'string' ? [message] : message;
+//   // use the filter method on a list of defined strings "test" and "test2" to check if the message is in the list, otherwise return the message
+//   var notmatch = messageArray.filter((test) => !test.includes("test") && !test.includes("test2") );
+
+//   res.send("notmatch: " + notmatch + " message: " + message);
+// };
+
+// // NotVulnerable: http://localhost:3000/echo3/test
+// // NotVulnerable: http://localhost:3000/echo3/abcd
+// // Vulnerability: http://localhost:3000/echo3/%3Cimg%20src=x%20onerror=alert(origin)%3E
+// app.get('/echo3/:message', echo3);
+
+// const echo4 = (unusedparam) => (req, res) => {
+//   const { keys: queryKeys } = req.query;
+//   const paramKeys = req.params;
+//   const message = queryKeys || paramKeys?.keys;
+
+//   if (!message) {
+//     res.status(400).send('No keys provided');
+//     return;
+//   }
+
+
+//   const messageArray = typeof message === 'string' ? [message] : message;
+//   // use the filter method on a list of defined strings "test" and "test2" to check if the message is in the list, otherwise return the message
+//   var notmatch = messageArray.filter((test) => !test.includes("test") && !test.includes("test2") );
+
+//   if (notmatch.length) {
+//     res.send("notmatch: " + notmatch + " message: " + message);
+//     return;
+//   }
+
+//   res.send("OK! Whitelist:"+unusedparam);
+
+// };
+
+// // OK - in whitelist: http://localhost:3000/echo4/test
+// // NOT - in whitelist: http://localhost:3000/echo4/abcd
+// // Vulnerability: http://localhost:3000/echo4/%3Cimg%20src=x%20onerror=alert(origin)%3E
+// app.get('/echo4/:keys', echo4(global_whitelist));
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!');
